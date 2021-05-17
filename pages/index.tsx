b37b03d8784b249101ca8aacaa7f2123f13b41e0
Lycolia Rizzim <lycolia@lycolia.info>
2021-05-17 20:08:29 +0900

    add return

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout';
 
-const IndexPage = () => (
-  <Layout title={'Hello Next.js'}>
-    <>
-      <h1>Hello Next.js 👋</h1>
-    </>
-  </Layout>
-);
+const IndexPage = () => {
+  return (
+    <Layout title={'Hello Next.js'}>
+      <>
+        <h1>Hello Next.js 👋</h1>
+      </>
+    </Layout>
+  );
+};
 
 export default IndexPage;
