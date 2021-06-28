35ea2892ba2dbb8569dc3ea98f0c9411ab96d120
Lycolia Rizzim <lycolia@lycolia.info>
2021-06-28 20:58:25 +0900

    upgrade testing , next

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json',
+    },
+  },
+  preset: 'ts-jest',
   roots: ['<rootDir>'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   watchPlugins: [
     'jest-watch-typeahead/filename',
