2b2ccb9654abdd48433217fe4859e04518921aad
Lycolia Rizzim <lycolia@lycolia.info>
2021-08-01 22:05:54 +0900

    add assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = (phase, { defaultConfig }) => {
   return {
     basePath: isProd ? '/deploy-path-here' : '',
+    assetPrefix: isProd ? '/assets-path-here' : '',
   };
 };
