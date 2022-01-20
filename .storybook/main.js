dea683e96e155ef071c10c9f794593928d35be6c
Lycolia Rizzim <lycolia@lycolia.info>
2022-01-20 23:29:48 +0900

    add SCSS support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
