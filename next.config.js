ab292a9261f85f662fae93482585f5b2ed56519a
Lycolia Rizzim <33796432+Lycolia@users.noreply.github.com>
2024-07-16 22:25:25 +0900

    ESLintのFlat Config化を断念、その他細かいアップデートなど (#20)
    * ESLint ConfigのFlat化を断念
    
    * エラーになるのでreact/react-in-jsx-scopeを追加
    
    * Next v14対応（仮）とpackage更新
    
    * 0.29.0

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = (phase, { defaultConfig }) => {
   return {
+    output: 'export',
     basePath: isProd ? '/deploy-path-here' : '',
     assetPrefix: isProd ? '/assets-path-here' : '',
-    pageExtensions: ['page.tsx'],
+    pageExtensions: ['page.tsx']
   };
 };
