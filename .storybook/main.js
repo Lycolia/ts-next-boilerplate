eadbdd75f12082d2a567567f3d59e24d7fdf88ce
Lycolia Rizzim <33796432+Lycolia@users.noreply.github.com>
2024-07-16 20:11:20 +0900

    modulesのアップデートと設定改善など (#18)
    * update storybook 7.5 -> 8.1
    
    * update Dependencies
    
    * ESLint Flat config対応
    
    * 設定を別リポジトリからバックポート
    
    * 0.27.0
    
    * warning解消
    
    * 勝手にアップデートされた内容を反映

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,19 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/preset-scss',
+    '@chromatic-com/storybook'
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,11 @@
 // import '../src/pages/_app.scss';
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+      date: /Date$/
+    }
+  }
 };
+export const tags = ['autodocs'];
